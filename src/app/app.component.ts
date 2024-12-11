dccf368961e1c870baccdab9898965700ff61db6
Jefferson <jefferosnjunior1645@gmail.com>
2024-12-11 13:50:05 -0300

    Footer feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,15 @@ import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './pages/home/home/home.component'
 import { NavbarComponent } from './components/navbar/navbar/navbar.component'
+import { FooterComponent } from "./components/footer/footer/footer.component"
+
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     HomeComponent,
     NavbarComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
