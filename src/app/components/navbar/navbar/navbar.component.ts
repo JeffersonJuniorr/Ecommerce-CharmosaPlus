11a047f02337c27de138d06ce1f8d212ef9959f2
Jhonn <jhonnbr4ndon@gmail.com>
2024-12-20 19:33:12 -0300

    Update code route

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isMenuOpen: boolean = false;
-
+ 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     const navLinks = document.querySelector('.navbar-links') as HTMLElement;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/pages/home/home/home.component';
+import { LoginComponent } from './app/pages/login/login/login.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '**', redirectTo: 'home' }
+    ]),
+  ],
+});
